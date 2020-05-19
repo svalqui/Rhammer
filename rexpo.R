42d066eb46fb6a8d00a2b61baf21769c645cc1ff
svalqui <svalqui@users.noreply.github.com>
2020-05-20 09:33:45 +1000

    Added Discover List content.

--- rexpo.R
+++ rexpo.R
@@ -13,3 +13,31 @@ DiscoverListNames <- function(ObjList, spacing = ""){
   } # End for
   
 } # End function
+
+
+DiscoverListContent <- function(ObjList, spacing = ""){
+  # Initilaize index counter
+  idx <- 0
+  spaces <- spacing
+  for (element in ObjList){
+    idx <- idx + 1
+    if (typeof(element) == "list"){
+      print(paste0(spaces, names(ObjList[idx])))
+      DiscoverListNames(element, "  ")
+    } else if (typeof(element) == "logical") {
+      print(paste0(spaces, names(ObjList[idx]), " - logical"))
+    } else if (typeof(element) == "integer") {
+      print(paste0(spaces, names(ObjList[idx]), " - integer"))
+    } else if (typeof(element) == "double") {
+      print(paste0(spaces, names(ObjList[idx]), " - double"))
+    } else if (typeof(element) == "complex") {
+      print(paste0(spaces, names(ObjList[idx]), " - complex"))
+    } else if (typeof(element) == "character") {
+      print(paste0(spaces, names(ObjList[idx]), " - character"))
+    } else if (typeof(element) == "raw") {
+      print(paste0(spaces, names(ObjList[idx]), " - raw"))
+    }
+
+  } # End for
+
+} # End function
