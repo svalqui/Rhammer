f5c1427e13421b4aa491a20864e901ca2c15e120
svalqui <svalqui@users.noreply.github.com>
2020-05-20 13:07:38 +1000

    Fix spacing.

--- rexpo.R
+++ rexpo.R
@@ -1,13 +1,13 @@
 DiscoverListNames <- function(ObjList, spacing = ""){
   # Initilaize index counter
   idx <- 0
-  spaces <- spacing
+  spacesLN <- spacing
+  spacesLN <- paste0(spacesLN, "  ")
   for (element in ObjList){
     idx <- idx + 1
     if (typeof(element) == "list"){
-      print(paste0(spaces, names(ObjList[idx])))
-      #print(spaces + names(ObjList[idx]))
-      DiscoverListNames(element, "  ")
+      print(paste0(spacesLN, names(ObjList[idx]), " - list on LN"))
+      DiscoverListNames(element, spacesLN)
     } # End if
     
   } # End for
@@ -18,24 +18,25 @@ DiscoverListNames <- function(ObjList, spacing = ""){
 DiscoverListContent <- function(ObjList, spacing = ""){
   # Initilaize index counter
   idx <- 0
-  spaces <- spacing
+  spacesLC <- spacing
   for (element in ObjList){
     idx <- idx + 1
     if (typeof(element) == "list"){
-      print(paste0(spaces, names(ObjList[idx])))
-      DiscoverListNames(element, "  ")
+      print(paste0(spacesLC, names(ObjList[idx]), " - list on LC"))
+
+      DiscoverListNames(element, spacesLC)
     } else if (typeof(element) == "logical") {
-      print(paste0(spaces, names(ObjList[idx]), " - logical"))
+      print(paste0(spacesLC, names(ObjList[idx]), " - logical on LC"))
     } else if (typeof(element) == "integer") {
-      print(paste0(spaces, names(ObjList[idx]), " - integer"))
+      print(paste0(spacesLC, names(ObjList[idx]), " - integer on LC"))
     } else if (typeof(element) == "double") {
-      print(paste0(spaces, names(ObjList[idx]), " - double"))
+      print(paste0(spacesLC, names(ObjList[idx]), " - double on LC"))
     } else if (typeof(element) == "complex") {
-      print(paste0(spaces, names(ObjList[idx]), " - complex"))
+      print(paste0(spacesLC, names(ObjList[idx]), " - complex on LC"))
     } else if (typeof(element) == "character") {
-      print(paste0(spaces, names(ObjList[idx]), " - character"))
+      print(paste0(spacesLC, names(ObjList[idx]), " - character on LC"))
     } else if (typeof(element) == "raw") {
-      print(paste0(spaces, names(ObjList[idx]), " - raw"))
+      print(paste0(spacesLC, names(ObjList[idx]), " - raw on LC"))
     }
 
   } # End for
@@ -44,25 +45,26 @@ DiscoverListContent <- function(ObjList, spacing = ""){
 
 
 DiscoverObj <- function(Obj, spacing = ""){
-  spaces <- spacing
+  spacesDO <- spacing
   if (typeof(Obj) == "list"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - list"))
-    spaces <- paste0(spaces, "  ")
-    for (element in Obj){
-      DiscoverObj(element, spaces)
-    } # End for
+    idx <- 0
+    print(paste0(spacesDO, deparse(substitute(Obj)), " - list On DO"))
+    spacesDO <- paste0(spacesDO, "  ")
+    DiscoverListContent(Obj, spacesDO)
+
   } else if (typeof(Obj) == "logical"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - logical"))
+    print(paste0(spacesDO, deparse(substitute(Obj)), " - logical On DO"))
   } else if (typeof(Obj) == "integer"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - integer"))
+    print(paste0(spacesDO, deparse(substitute(Obj)), " - integer On DO"))
   } else if (typeof(Obj) == "double"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - double"))
+    print(paste0(spacesDO, deparse(substitute(Obj)), " - double On DO"))
   } else if (typeof(Obj) == "complex"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - complex"))
+    print(paste0(spacesDO, deparse(substitute(Obj)), " - complex On DO"))
   } else if (typeof(Obj) == "character"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - character"))
+    print(paste0(spacesDO, deparse(substitute(Obj)), " - character On DO"))
   } else if (typeof(Obj) == "raw"){
-    print(paste0(spaces, deparse(substitute(Obj)), " - raw"))
+    print(paste0(spacesDO
+      , deparse(substitute(Obj)), " - raw On DO"))
   }
 
 } # End function
